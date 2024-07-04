e3146121c6e2a433dbf1bec72bb08ecec26dcc6d
dmitr <dmitrijsattarov@gmail.com>
2024-07-04 05:33:39 +0500

    add second homework

---
+++ lesson_6/lesson_6_task_1.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+chrome = webdriver.Chrome()
+wait = WebDriverWait(chrome,40,0.1)
+firefox = webdriver.Firefox()
+wait = WebDriverWait(firefox,40,0.1)
+try:
+
+    chrome.get("http://uitestingplayground.com/ajax")
+    firefox.get("http://uitestingplayground.com/ajax")
+    blue_button = chrome.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+    text_from_content = wait.until(EC.visibility_of_all_elements_located(
+            (By.CSS_SELECTOR, ".bg-success"))).text
+    blue_button = firefox.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
+    text_from_content = wait.until(EC.visibility_of_all_elements_located(
+            (By.CSS_SELECTOR, ".bg-success"))).text
+    print(text_from_content)
+except Exception as ex:
+        print(ex)
+finally:
+    chrome.quit()
+    firefox.quit()
