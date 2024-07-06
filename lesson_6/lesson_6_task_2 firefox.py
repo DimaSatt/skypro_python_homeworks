2c31e7cf3da02426112134593c93726a913b190a
dmitr <dmitrijsattarov@gmail.com>
2024-07-06 05:21:23 +0500

    add second homework

--- lesson_6/lesson_6_task_2 firefox.py
+++ lesson_6/lesson_6_task_2 firefox.py
@@ -1,15 +1,11 @@
 from selenium import webdriver
 
 
-chrome = webdriver.Chrome()
+
 firefox = webdriver.Firefox()
 try:
-    chrome.get("http://uitestingplayground.com/textinput")
+   
     firefox.get("http://uitestingplayground.com/textinput")
-    button_name = chrome.find_element(
-        "id", "newButtonName").send_keys("SkyPro")
-    confirm_button_name = chrome.find_element("id","updatingButton").click()
-    new_button_name = chrome.find_element("id","updatingButton").text
     button_name = firefox.find_element(
         "id", "newButtonName").send_keys("SkyPro")
     confirm_button_name = firefox.find_element("id","updatingButton").click()
@@ -19,5 +15,4 @@ try:
 except Exception as ex:
         print(ex)
 finally:
-    chrome.quit()
-    firefox.quit()    
+       firefox.quit()    
